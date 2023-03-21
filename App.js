92f5e96272edf7e2e91d8001b91d89826096c3ed
zgbsss <gabrielasaraiva.04@gmail.com>
2023-03-21 15:13:44 -0300

    criação do projeto

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const livro = "https://m.media-amazon.com/images/I/91M9xPIf10L.jpg"
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{fontSize: 30, fontWeight: 'bold'}}>Livros</Text>
+      <View style={styles.conteudo}>
+        <View style={styles.card}>
+          <Image style={{ resideMode: 'stretch', height: 200}} source={{uri:livro}} />
+          <Text>Título</Text>
+          <Text>Preço</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 10,
+    marginTop: 40,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  conteudo: {
+    flex: 1,
+    backgroundColor: '#ccc',
+    paddingTop: 20,
+    alignItems: 'center',
+    width: '100%',
+  },
+  card: {
+    width: '40%',
+    height: 240,
+    backgroundColor: '#7fa331',
+    borderRadius: 10,
   },
 });
